137c1a8aa9dc03fa568625d800f8071d8d2ba61c
Shivam Hingorani <shivam_hingorani@brown.edu>
2025-07-03 10:09:19 -0400

    set up vite + tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,4 +15,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
